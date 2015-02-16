a90bec82c10984fc5ecb59a99ba88dc199c8f281
Mark Daniels <mark_daniels1982@hotmail.com>
2015-02-16 12:35:33 -0800

    install ejs,body-parser,pg,sequelize dependencies/createdb/sequelize

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
+/*DEPENDENCIES*/
+
+/*app routing*/
 var express = require("express");
 
+/*embedded javascript*/
+var ejs = require("ejs");
+
+/*JSON parser*/
+var bodyParser = require("body-parser");
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(bodyParser.json())
+
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
+
+/*postgresql database*/
+var pg = require("pg");
+
+/*Object Relational Mapper*/
+var sequelize = require("sequelize");
+
+/*application init*/
 var app = express();
 
+/*root path*/
 app.get('/', function (req, res) {
   res.render('site/home.ejs')
 });
