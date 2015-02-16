fe67ace6e6f01faad9aafef823fe3472fde37414
Mark Daniels <mark_daniels1982@hotmail.com>
2015-02-16 13:50:57 -0800

    logo font change

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ var ejs = require("ejs");
 var bodyParser = require("body-parser");
 
 //parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+// app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
+// })
 
 /*postgresql database*/
 var pg = require("pg");
